56cbbe3b582bf0b01463231bc3854155944fb015
rahulyadav8058 <rahul.yadav23b@iiitg.ac.in>
2026-08-07 23:29:02 +0530

    Add: find-median-in-a-data-stream - submission-0

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-0.cpp
@@ -0,0 +1,37 @@
+class MedianFinder {
+    priority_queue<int>max_heap;
+    priority_queue<int,vector<int>,greater<int>>min_heap;
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        max_heap.push(num);
+        
+        min_heap.push(max_heap.top());
+        max_heap.pop();
+
+        if(min_heap.size()>max_heap.size()){
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+        }
+    }
+    
+    double findMedian() {
+        double ans =0;
+        if(min_heap.size()==max_heap.size()){
+            ans += min_heap.top();
+            ans += max_heap.top();
+            return ans/2;
+        }
+        return max_heap.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
